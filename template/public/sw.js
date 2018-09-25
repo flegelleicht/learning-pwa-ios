3dca7b564cb66fb04e109cfa0a597a96629ea7fe
Erik Hebisch <flegelleicht@gmail.com>
2018-09-25 15:23:46 +0200

    Cached resources and became active service worker on activation

--- template/public/sw.js
+++ template/public/sw.js
@@ -7,3 +7,17 @@ self.addEventListener('install', () => {
   return self.skipWaiting();
 });
 
+self.addEventListener('activate', () => {
+  event.waitUntil(
+    caches.open('wiederlist')
+      .then((cache) => {
+        return cache.addAll(cachedUrls);
+      })
+  )
+  .then(() => {
+    return self.clients.claim();
+  })
+  .catch((error) => {
+    console.log(`Error during cache handling: ${error}`);
+  })
+})
