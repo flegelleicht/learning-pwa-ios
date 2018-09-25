186c473a95060887fffb44866bd8da7819b2c596
Erik Hebisch <flegelleicht@gmail.com>
2018-09-25 16:59:00 +0200

    Updated shebang to start directly with bundle exec

--- template/secure-local-serve.rb
+++ template/secure-local-serve.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env bundle exec ruby
 #
 # This code snippet shows how to enable SSL in Sinatra.
 # Thank you to: https://gist.github.com/TakahikoKawasaki/40ef0ab011b0a467bedf#file-sinatra-ssl-rb
