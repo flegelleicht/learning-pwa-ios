548f91646c3fa27f160e55a8a25338b9055c0e88
Erik Hebisch <flegelleicht@gmail.com>
2018-09-25 15:42:33 +0200

    Fetched resources from cache

--- template/public/sw.js
+++ template/public/sw.js
@@ -21,3 +21,23 @@ self.addEventListener('activate', () => {
     console.log(`Error during cache handling: ${error}`);
   })
 })
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if(response) { return response; }
+
+        return fetch(event.request)
+                .then((response) => {
+                  return caches.open('wiederlist')
+                          .then((cache) => {
+                            cache.put(event.request, response.clone());
+                            return response;
+                          });
+                });
+      })
+      .catch((error) => {
+        console.log(`Error fetching resource from cache: ${error.message}`);
+      })
+  );
+});
