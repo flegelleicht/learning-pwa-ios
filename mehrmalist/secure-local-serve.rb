14d3e0ac7a1b733525893e9561149b48799d2335
Erik Hebisch <flegelleicht@gmail.com>
2018-09-29 20:45:36 +0200

    Added server code for local development

---
+++ mehrmalist/secure-local-serve.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env bundle exec ruby
+#
+# This code snippet shows how to enable SSL in Sinatra.
+# Thank you to: https://gist.github.com/TakahikoKawasaki/40ef0ab011b0a467bedf#file-sinatra-ssl-rb
+
+require 'sinatra/base'
+
+class Application < Sinatra::Base
+  configure do
+    set :bind, '0.0.0.0'
+    set :port, 3003
+  end
+
+  get '/' do
+    'Hello, SSL.'
+  end
+
+  def self.run!
+    super do |server|
+      server.ssl = true
+      server.ssl_options = {
+        :cert_chain_file  => File.dirname(__FILE__) + "/server.crt",
+        :private_key_file => File.dirname(__FILE__) + "/server.key",
+        :verify_peer      => false
+      }
+    end
+  end
+
+  run! if app_file == $0
+end
